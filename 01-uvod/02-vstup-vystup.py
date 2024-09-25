b250861d1cc789d3378fed0fe364b8aaddf13739
Michal Šutek <sutek.spsit@gmail.com>
2024-09-25 09:15:26 +0200

    Add vstup-vystupand vypocty to 01-uvod

---
+++ 01-uvod/02-vstup-vystup.py
@@ -0,0 +1,7 @@
+a = 100 # = je príkaz priradenia, tzn. že v premennej a bude hodnota 10
+print(a) # print je príkaz výstupu, vypíše do terminálu obsah premennej a
+b = 200
+print("V premennej b je hodnota:") # ako chceme vypísať text, dávame úvodzovky, ak chceme vypísať obsah premennej, dávame len názov premennej (bez úvodzoviek)
+print(b)
+print("V premennej b je hodnota:", b) # ak kombinujeme pri výpise reťazec s premennou, oddeľujeme čiarkami
+print("V premennej a je hodnota:", a, "a v premennej b je hodnota:", b)
---
+++ 01-uvod/03-vypocty.py
@@ -0,0 +1,10 @@
+a = int(input("Zadaj číslo a: ")) # Input je príkazom vstupu. Vypíše na obrazovku text v zátvorke a to čo užívateľ napíše na klávesnici, uloží do premennej a
+b = int(input("Zadaj číslo b: ")) # input vracia vždy výsledok vo formáte reťazca (postupnosť znakov resp. text), funkcia int() pretypuje reťazec na celé číslo
+sucet = a + b
+print("Súčet je:", sucet)
+sucin = a * b
+print("Súčin je:", sucin)
+rozdiel = a - b
+print("Rozdiel je:", rozdiel)
+podiel = a / b
+print("Podiel je:", podiel)
